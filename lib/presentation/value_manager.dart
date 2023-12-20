865b9cde2aabfe7230dd59958a1cf231bebfbe24
SaloniPardeshi1412 <saloni.p@binyuga.com>
2023-12-20 11:12:07 +0530

    Presentation layer added.

---
+++ lib/presentation/value_manager.dart
@@ -0,0 +1,56 @@
+class AppMargin {
+  static const double m8 = 8.0;
+}
+
+class AppPadding {
+  static const double p2 = 2.0;
+  static const double p8 = 8.0;
+  static const double p10 = 12.0;
+  static const double p20 = 20.0;
+  static const double p25 = 25.0;
+  static const double p30 = 30.0;
+  static const double p35 = 35.0;
+  static const double p58 = 58.0;
+  static const double p100 = 100.0;
+  static const double p130 = 130.0;
+  static const double p150 = 150.0;
+  static const double p200 = 180.0;
+  static const double p250 = 250.0;
+  static const double p280 = 280.0;
+  static const double p430 = 430.0;
+  static const double p650 = 650.0;
+  static const double p1000 = 1000.0;
+}
+
+class AppSize {
+  static const double s0 = 0;
+  static const double s10 = 10;
+  static const double s15 = 15;
+  static const double s16= 16;
+  static const double s20 = 20;
+
+  static const double s40 = 40;
+  static const double s50 = 50;
+  static const double s60 = 60;
+  static const double s70 = 70;
+  static const double s80 = 80;
+  static const double s100 = 100;
+  static const double s120 = 120;
+  static const double s130 = 130;
+  static const double s160 = 160;
+  static const double s200 = 200;
+  static const double s250 = 250;
+  static const double s308 = 308;
+  static const double s320 = 320;
+  static const double s380 = 380;
+  static const double s600 = 600;
+  static const double s636 = 636;
+  static const double s500 = 500;
+  static const double s870 = 870;
+  static const double s900 = 900;
+  static const double s1000 = 1000;
+  static const double s2200 = 2200;
+  static const double s720 = 720;
+  static const double s780 = 780;
+}
+
