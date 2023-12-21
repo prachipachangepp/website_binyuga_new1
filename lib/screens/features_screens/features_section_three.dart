f2c118fc82622fa8ff24a9803ac6836d8efa4e9d
HP <kajaldewade9700@gmail.com>
2023-12-21 10:35:14 +0530

    Updated Screens add images and constant

---
+++ lib/screens/features_screens/features_section_three.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../presentation/string_manager.dart';
+import '../../presentation/theme_manager.dart';
+import '../../presentation/value_manager.dart';
+
+class FeatureSectionThree extends StatelessWidget {
+  const FeatureSectionThree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child:
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.network(
+                "images/Frame 14.png",
+                height: AppSize.s636,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width / 1.5,
+                // width:200,
+              ),
+              SizedBox(height:10,),
+              Padding(
+                padding: const EdgeInsets.only(right:250),
+                child: Image.network(
+                  "images/design 2.png",
+                  height: AppSize.s500,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width / 2,
+                  // width:200,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
