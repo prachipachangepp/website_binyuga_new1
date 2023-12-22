3aaebf20437eeb5fd62654e10fc02824a6918fc8
DELL <patipuja186@gmail.com>
2023-12-22 15:30:33 +0530

    updated feature page

--- lib/screens/features_screens/features_section_two.dart
+++ lib/screens/features_screens/features_section_two.dart
@@ -22,7 +22,7 @@ class FeatureSectionTwo extends StatelessWidget {
               Image.network(
                 "images/design 1.png",
                 height:AppSize.s380,
-                width: MediaQuery.of(context).size.width/2.3,
+                width: MediaQuery.of(context).size.width/2.6,
               ),
             ],
           ),
